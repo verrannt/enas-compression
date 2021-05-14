729f2ebdf8afca123713b2ea2788596640048782
verrannt <pa.schroeder@posteo.net>
2021-05-14 12:58:25 +0200

    implement function to compute embeddings on a batch

---
+++ src/embeddings.py
@@ -0,0 +1,25 @@
+
+from torch import nn
+
+def _get_subnet(parent_net, break_layer_idx = '1'):
+    """
+    Get subnet from given network at index
+    """
+    children_list = []
+    for n,c in parent_net.named_children():
+        children_list.append(c)
+        if n == break_layer_idx:
+            break
+    sub_net = nn.Sequential(*children_list)
+    return sub_net
+
+def get_embeddings(batch, network, layers):
+    subnets = []
+    for layer_id in layers:
+        subnets.append(_get_subnet(network, layer_id))
+
+    embeddings = []
+    for subnet in subnets:
+        embeddings.append(sub_net1(batch).detach().numpy())
+
+    return embeddings
